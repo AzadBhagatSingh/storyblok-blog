e5799859c7d8dd213df09559059e04f1729deb7a
AzadBhagatSingh <singhaazad@gmail.com>
2019-08-16 11:11:18 +0530

    about page and basic styles added

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,14 +5,15 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: "Storybolk + Nuxt = <3",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: "An awesome blog about Nuxtjs and Storyblok" }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Lato:400,700&display=swap' }
     ]
   },
   /*
---
+++ pages/about/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <section>
+      <h1>Hi! that's us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur voluptates hic perferendis nam quae, deserunt nihil in, ratione natus labore dolor mollitia? Necessitatibus asperiores provident ut suscipit voluptate, illum repellat.</p>
+  </section>
+</template>
