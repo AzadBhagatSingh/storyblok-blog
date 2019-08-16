25adfa5d8562e9f671ca17e1b558b48eae34945a
AzadBhagatSingh <singhaazad@gmail.com>
2019-08-16 13:35:47 +0530

    frontend prepared

---
+++ components/Blog/PostPreview.vue
@@ -0,0 +1,70 @@
+<template>
+  <nuxt-link :to="id">
+      <article class="post-preview">
+        <div
+          :style="{backgroundImage: 'url(' + thumbnailImage + ')'}"
+          class="post-preview-thumbnail">
+          <div class="post-preview-content">
+            <h1>{{ title }}</h1>
+            <p>{{ excerpt }}</p>
+          </div>
+        </div>
+      </article>
+    </nuxt-link>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    excerpt: {
+      type: String,
+      required: true
+    },
+    thumbnailImage: {
+      type: String,
+      required: true
+    },
+    id: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+  color: #000;
+}
+
+.post-preview {
+  border-radius: 3px;
+  box-shadow: 1px 1px 5px 1px rgb(0, 0, 0, 0.5);
+  width: 90%;
+  height: 20rem;
+  margin: 1rem;
+}
+
+.post-preview-thumbnail {
+  background-position: center;
+  background-size: cover;
+  width: 100%;
+  height: 10rem;
+}
+
+.post-preview-content {
+  text-align: center;
+  padding: 10rem;
+}
+
+@media (min-width: 35rem) {
+  .post-preview {
+    width: 25rem;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,55 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        storyblok-blog
-      </h1>
-      <h2 class="subtitle">
-        Awesome blog
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <section id="posts">
+    <PostPreview
+      v-for="post in posts"
+      :key="post.id"
+      :title="post.title"
+      :excerpt="post.previewText"
+      :thumbnailImage="post.thumbnailUrl"
+      :id="post.id" />
+  </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import PostPreview from '@/components/Blog/PostPreview'
 
 export default {
   components: {
-    Logo
+    PostPreview
+  },
+  data () {
+    return {
+      posts: [
+        {
+          title: 'Paleo Pizza',
+          previewText: 'This will be awesome!',
+          thumbnailUrl: 'https://www.washingtonpost.com/resizer/M-WnldvRmvg3qyWg0Om8ssM6E3M=/1484x0/arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/UM4VXMMJ5Y7T5MPQALWPL73RGM.jpg',
+          id: 'paleo-pizza'
+        },
+        {
+          title: 'Flatbread Pizza',
+          previewText: 'This will be awesome!',
+          thumbnailUrl: 'https://www.washingtonpost.com/resizer/M-WnldvRmvg3qyWg0Om8ssM6E3M=/1484x0/arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/UM4VXMMJ5Y7T5MPQALWPL73RGM.jpg',
+          id: 'flatbread-pizza'
+        }
+      ]
+    }
   }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
+<style scoped>
+#posts {
+  padding-top: 2rem;
   display: flex;
   justify-content: center;
   align-items: center;
-  text-align: center;
+  flex-direction: column;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+@media (min-width: 35rem) {
+  #posts {
+    flex-direction: row;
+  }
 }
 </style>
